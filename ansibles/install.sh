292db8a4e527e607ffa9e5cb1e2c9a2534682da7
Bluffmaster14 <akashkarale14@gmail.com>
2025-06-05 18:03:29 +0530

    anisbles addition

--- ansibles/install.sh
+++ ansibles/install.sh
@@ -1,7 +1,7 @@
 env=$1
 
-cp inventory_file $pwd/files/environment/$env/
-cp param.yml $pwd/files/environment/$env/group_vars/all/
+cp inventory_file ${pwd}/files/environment/$env/
+cp param.yml {$pwd}/files/environment/$env/group_vars/all/
 
 ansible-playbook -i files/environment/$env/inventory_file files/install.yml
 
